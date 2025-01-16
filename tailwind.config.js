4380f6c020eb533be1ceff2f8966e766c8791a8d
Crystal <crystalyeo123@hotmail.com>
2025-01-16 23:29:32 +1300

    add common colors, set screen size in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: '#0c0c0c',
+        navy: '#000124',
+        blue: '#0F45DB',
+        'dark-grey': '#181818',
+        'light-grey': '#adb5bd',
+        white: '#f3f3f3',
+      },
+      sm: '640px', // Large Mobile
+      md: '768px', // Tablet
+      lg: '1024px', // Desktop
+      xl: '1280px', // Large Desktop
+    },
   },
   plugins: [],
 };
