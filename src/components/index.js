5f267b24433b4062f12960b8f8119316a9ee303c
Crystal <crystalyeo123@hotmail.com>
2025-01-16 23:03:50 +1300

    create components

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Hero from './Hero';
+import Navbar from './Navbar';
+import Skill from './Skill';
+import Experience from './Experience';
+import Project from './Project';
+import Contact from './Contact';
+
+export { Hero, Navbar, Skill, Experience, Project, Contact };
